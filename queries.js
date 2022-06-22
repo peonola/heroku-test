efe17106a89bdd471f2694c8b7a05ca3c643594e
peonola <kocabaybugra@gmail.com>
2022-06-22 17:08:03 +0300

    change db config

--- queries.js
+++ queries.js
@@ -1,21 +1,26 @@
-const { error } = require("console");
-const { response } = require("express");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const app = require("./index");
 
 const Pool = require("pg").Pool;
 
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "postgres",
+//   password: "postgres",
+//   port: 5433,
+// });
+
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "postgres",
-  password: "postgres",
-  port: 5433,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`App running....`);
+});
 
 module.exports = pool;
